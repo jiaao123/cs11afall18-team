26935a68e67410b720afebb2075c21cd3f0fc1fd
klydia16 <44778986+klydia16@users.noreply.github.com>
2018-12-05 15:12:03 -0500

    Update Bingo.java

--- Bingo.java
+++ Bingo.java
@@ -1,191 +1,108 @@
-public class BingoDemo{
-public static int[] rows = new int[50];
-public static int[] columns= new int[50];
-public static int count=0;
+import java.util.Random;
+import java.util.Scanner;
 
-  public static void main (String[]args){
-	int number=1;
-  int countNumber=0;
-  int range=0;
-	//play the game
-  System.out.println("Enter the range of the board");
-  range=TextIO.getlnInt();
-  int[] board=bingoBoard(range);
-
-	while (number!=0){
-	  System.out.println("Enter a number or enter 0 to quit");
-  	number = TextIO.getlnInt();
-    countNumber++;
-    if(number!=0){
-    test(board,number);
-    checkEverything(rows,columns);
-     }
-    }
-
-    System.out.println("The board is "+java.util.Arrays.toString(board));
-    if(countNumber==0&&number==0){
-    System.out.println("Game closed");
-  }
-  for(int i=1;i<6;i++){
-    for(int a=1;a<6;a++){
-      int pos=a*i-1;
-      System.out.print(board[pos]+" ");
-    }
-    System.out.println();
-  }
-}//end of main;
-
-public static void checkEverything(int[] rows,int[] columns){
-  int operator1=0;
-  int operator2=0;
-  int operator3=0;
-  operator1 = checkRows(rows);
-  operator2 = checkColumns(columns);
-  operator3 = checkDiagonal(rows,columns);
+public class BingoGame{
+  public static int[] location=new int[25];
+  public static int count=0;
 
 
-  if(operator1==1||operator2==1||operator3==1){
-    print();
-  }
-  else{
-    System.out.println("no bingo");
-  }
+  public static void main(String[] args){
+    int number=1;
+    int start=1;
+    Scanner sc = new Scanner(System.in);
+    while (start!=0)
+    System.out.println("What is the range of the bingo board?");
+    int range=sc.nextInt();
 
-}
-
-  public static int[] bingoBoard(int range){
-    int temp=0;
-    int[] Board = new int[25];
-    java.util.Random rand = new java.util.Random();
-    for(int i=0;i<Board.length;i++){
-      temp=(rand.nextInt(range));
-      Board[i]=temp+1;
-    }
-    return Board;
-  }//end of bingoBoard
-
-public static int checkRows(int[] rows){
-    int counter0=0;
-    for(int rowNumber=1;rowNumber<6;rowNumber++){
-      int counter =0;
-      for(int i=0;i<rows.length;i++){
-        if(rows[i]==rowNumber){
-          counter++;
-          if(counter==5){
-            counter0++;
+    int[] board=boardGenerator(range);
+    while (number!=0){
+     System.out.println("Enter a number or enter 0 to quit");
+     number = TextIO.getlnInt();
+      test(number,board);
+      printBoard(board);
+      boolean operator=check();
+      if(operator){
+        System.out.printf("%n Bingo %n");
+        printBingo();
+        number=0;
       }
-     }
-     }
-    }
-  if(counter0>0){
-    return 1;
-  }
-  else{
-    return 0;
-  }
-}
+    }//end of while
 
-public static int checkColumns(int[] columns){
-    int counter0 =0;
-    for(int columnNumber=1;columnNumber<6;columnNumber++){
-    int counter=0;
-      for(int i=0;i<columns.length;i++){
-        if(columns[i]==columnNumber){
-          counter++;
-          if(counter==5){
-            counter0++;
-         }
-       }
+  }//end of main
+public static void test(int number,int[] board){
+   for(int i=0;i<board.length;i++){
+     if(number==board[i]){
+       location[i]=100;
      }
-    }
-    if(counter0>0){
-      return 1;
-  }
-  else{
- return 0;
-}
-}
-
-public static int checkDiagonal(int[] rows,int[] columns){
-  int counter1=0;
-  int counter2=0;
-    for(int i=1;i<6;i++){
-      if(rows[i]==columns[i]){
-        counter1++;
-      }
-      if(rows[i]==(6-columns[i])){
-        counter2++;
-      }
-    }
-    if(counter1==5||counter2==5){
-    return 1;
    }
-  else{
-    return 0;
-  }
-}
-
-
-
-public static void test(int[] board,int number){
-  int location[]=new int[25];
-  int counter=0;
-  for(int i=0;i<board.length;i++){
-    if(number==board[i]){
-      int temp4=i;
-      location[counter]=temp4;
-      counter++;
-    }
-  }
-  if(counter>0){
-    locationToPoint(location,counter);
-  }
-  else{
-    System.out.println("no match");
-    }
 }//end of test
 
-public static void locationToPoint(int[] location,int counter){
+public static boolean check(){
+  if(location[0]==100&&location[5]==100&&location[10]==100&&location[15]==100&&location[20]==100){
+      return true;
+    }else if(location[1]==100&&location[6]==100&&location[11]==100&&location[16]==100&&location[21]==100){
+      return true;
+    }else if(location[2]==100&&location[7]==100&&location[12]==100&&location[17]==100&&location[22]==100){
+      return true;
+    }else if(location[3]==100&&location[8]==100&&location[13]==100&&location[18]==100&&location[23]==100){
+      return true;
+    }else if(location[4]==100&&location[9]==100&&location[14]==100&&location[19]==100&&location[24]==100){
+      return true;
+    }else if(location[0]==100&&location[1]==100&&location[2]==100&&location[3]==100&&location[4]==100){
+      return true;
+    }else if(location[5]==100&&location[6]==100&&location[7]==100&&location[8]==100&&location[9]==100){
+      return true;
+    }else if(location[10]==100&&location[11]==100&&location[12]==100&&location[13]==100&&location[14]==100){
+      return true;
+    }else if(location[15]==100&&location[16]==100&&location[17]==100&&location[18]==100&&location[19]==100){
+      return true;
+    }else if(location[20]==100&&location[21]==100&&location[22]==100&&location[23]==100&&location[24]==100){
+      return true;
+    }else if(location[0]==100&&location[6]==100&&location[12]==100&&location[18]==100&&location[24]==100){
+      return true;
+    }else if(location[4]==100&&location[8]==100&&location[12]==100&&location[16]==100&&location[20]==100){
+      return true;
+    }else{
+      return false;
+    }
 
-  int a=0;
-  while(a<counter){
-    int lo=location[a];
-    int row=0;
-    int column=0;
-    if(0<=lo&&lo<=4){
-      row=1;column=lo+1;
-    }
-    else if(5<=lo&&lo<=9){
-      row=2;column=lo-5+1;
-    }
-    else if(10<=lo&&lo<=14){
-      row=3;column=lo-10+1;
-    }
-    else if(15<=lo&&lo<=19){
-      row=4;column=lo-15+1;
+}//end of check
+
+public static void printBoard(int[] board){
+  System.out.println();
+for(int i=0;i<5;i++){
+  for(int a=0;a<5;a++){
+    if(location[a+i*5]==100){
+      System.out.printf("%4d",board[a+i*5]);
     }
     else{
-      row=5;column=lo-20+1;
+      System.out.print("   *");
     }
-    rows[count]=row;
-    columns[count]=column;
-    count++;
-    System.out.println("("+row+","+column+")");
-    a++;
   }
+  System.out.println();
+}
+}//end of printBoard
 
-}//end of locationToPoint
-
-  public static void print(){
-    System.out.println("*   *            *****         *             *             *********            *********                 ");
-    System.out.println("*       *          *           * *           *           *                    *           *    ");
-    System.out.println("*         *        *           *   *         *          *                    *             *           ");
-    System.out.println("*        *         *           *     *       *          *                    *             *        ");
-    System.out.println("* *    *           *           *       *     *          *                    *             *        ");
-    System.out.println("*        *         *           *         *   *          *        *****       *             *       ");
-    System.out.println("*          *       *           *           * *          *            *       *             *       ");
-    System.out.println("*         *        *           *             *           *           *        *           *");
-    System.out.println("*     *          *****         *             *             **********           *********             ");
-
+public static int[] boardGenerator(int range){
+  Random rand = new Random();
+  int[] board=new int[25];
+  for(int i=0;i<25;i++){
+    int temp1=(rand.nextInt(range));
+    board[i]=temp1+1;
   }
-}//end of the class
+  return board;
+}//end of board Generator;
+
+public static void printBingo(){
+  System.out.println("*   *            *****         *             *             *********            *********                 ");
+  System.out.println("*       *          *           * *           *           *                    *           *    ");
+  System.out.println("*         *        *           *   *         *          *                    *             *           ");
+  System.out.println("*        *         *           *     *       *          *                    *             *        ");
+  System.out.println("* *    *           *           *       *     *          *                    *             *        ");
+  System.out.println("*        *         *           *         *   *          *        *****       *             *       ");
+  System.out.println("*          *       *           *           * *          *            *       *             *       ");
+  System.out.println("*         *        *           *             *           *           *        *           *");
+  System.out.println("*     *          *****         *             *             **********           *********             ");
+  }
+}
+//end of class
